1fb7da75d28b26136b57091b8fd3c03db13b7dd8
MasterSun8 <kacpi.doro@gmail.com>
2022-02-17 10:59:36 +0100

    i don't want google to think i'm ddosing them, added sleep 1s

--- some.py
+++ some.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-from logging import error
-from tokenize import String
 from pythonping import ping
 import os
+import time
 
 if os.path.exists("ping.txt"):
     os.remove("ping.txt")
@@ -11,13 +10,15 @@ print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('ping.txt', 'a'))
 
 while True:
     try:
-            fullstring = ping('8.8.8.8', count=1).__str__()
-            y = 'Req'
-            if y in fullstring:
-                print('Packet loss. Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
-            elif float(fullstring[31:36]) > 99.0:
-                print('Ping too high.(', fullstring[31:35], ') Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
-            else:
-                print(fullstring[31:36], file=open('ping.txt', 'a'))
+        time.sleep(1)
+        fullstring = ping('8.8.8.8', count=1).__str__()
+        y = 'Req'
+        if y in fullstring:
+            print('Packet loss. Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
+        elif float(fullstring[31:36]) > 99.0:
+            print('Ping too high.(', fullstring[31:35], ') Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
+        else:
+            print(fullstring[31:36], file=open('ping.txt', 'a'))
+
     except Exception as err:
-            print('{}'.format(err), file=open('except.txt', 'a'))
+        print('{}'.format(err), file=open('except.txt', 'a'))
