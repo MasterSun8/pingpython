0b6a69177aece691ac499f79e36440d33a05a5e1
MasterSun8 <kacpi.doro@gmail.com>
2022-02-24 17:12:13 +0100

    fully functional script, just throw the batch shortcut into autostart and you are free to check it whenever you want before restarting the computer

--- ping.py
+++ ping.py
@@ -3,10 +3,14 @@ from pythonping import ping
 import os
 import time
 
+print('uwu')
+
 if os.path.exists("ping.txt"):
     os.remove("ping.txt")
+if os.path.exists("except.txt"):
+    os.remove("except.txt")
 
-print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('ping.txt', 'a'))
+print("\n\n", datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), end='', file=open('ping.txt', 'a'))
 
 while True:
     try:
@@ -14,11 +18,10 @@ while True:
         fullstring = ping('8.8.8.8', count=1).__str__()
         y = 'Req'
         if y in fullstring:
-            print('Packet loss. Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
+            print('\nPacket loss. Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
         elif float(fullstring[31:36]) > 99.0:
-            print('Ping too high.(', fullstring[31:35], ') Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
+            print('Ping too high.(', fullstring[32:34], ') Gleba:', datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), file=open('gleba.txt', 'a'))
         else:
             print(fullstring[31:36], file=open('ping.txt', 'a'))
-
     except Exception as err:
         print('{}'.format(err), file=open('except.txt', 'a'))
---
+++ pinging.bat
@@ -0,0 +1,3 @@
+ECHO OFF
+start pythonw.exe ./ping.py
+EXIT
